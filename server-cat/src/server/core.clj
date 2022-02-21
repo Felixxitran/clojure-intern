639ffef152f33fd2901e1d12a48b54bbfc76c0fe
Felixxitran <khoaisme123@gmail.com>
2022-02-20 21:38:34 -0800

    connected to mongo

--- server-cat/src/server/core.clj
+++ server-cat/src/server/core.clj
@@ -3,12 +3,44 @@
   (:require [compojure.core :refer [defroutes GET POST]]
             [compojure.route :as route]
             [org.httpkit.server :refer [run-server]]
-            [cheshire.core :as json]))
+            [cheshire.core :as json]
+            [monger.core :as mg]
+            [monger.collection :as mc]
+            )
+   (:import [org.bson.types ObjectId]
+           [com.mongodb DB WriteConcern]))
+;;connect to mongodb database
+
+  (let [conn (mg/connect)
+        db   (mg/get-db conn "monger-test")]
+
+
+(defn counting []
+  (println "counted"))
+
+
+(defn change-data [db]
+  (mc/insert db "countries" {:name "Germany" :official-language "German" :population 82000000}))
+(defn get-data [db]
+   (mc/find db "countries" {:name "Germany"})
+  (mc/find-maps db "countries" {:name "Germany"}))
+(defn remove-data [db]
+  (mc/remove db "countries" {:name "Germany"}))
+
 
 (defroutes app
-  (GET "/" [] "Hello world"))
+  (GET "/cat" [] "Hello world")
+  (GET "/get-data" [db] (get-data [db]))
+  (POST "/remove-data" [db] (remove-data [db]))
+  )
+(defn start-server []
+  
+  (counting)
+  (run-server app {:port 3000})))
+
 (defn -main
   []
-  (run-server app {:port 3000}))
+  (start-server)
+  )
 
 
