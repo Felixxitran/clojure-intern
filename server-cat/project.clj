6029437ba66f243f9a28ff1e10ed1bb0275a0a19
Felixxitran <khoaisme123@gmail.com>
2022-02-22 03:05:00 -0800

    GET ok but not POST and no params

--- server-cat/project.clj
+++ server-cat/project.clj
@@ -4,9 +4,10 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [com.novemberain/monger "3.5.0" ]
+                 [com.novemberain/monger "3.5.0"]
                  [compojure "1.6.2"]
-                 ]
+                 [ring/ring-core "1.7.1"]
+                 [ring/ring-jetty-adapter "1.7.1"]]
   :main ^:skip-aot server-cat.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
